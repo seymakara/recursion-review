8e77df1c53bf4073d84478d721180077126614b0
seymakara <seymakara88@gmail.com>
2018-11-05 14:26:50 -0800

    completed stringifyJSON task

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,58 @@
 
 // but you don't so you're going to write it from scratch:
 
+/*
+  if it is null || not an object{
+    if it is a string{
+      return '"' + obj + '"'
+    }
+    else{
+      return '' + obj
+    }
+  }
+  else if Array.isArray(obj){
+    var str = ''
+    for item in Array{  
+        str += stringifyJSON(obj[i])  
+    return '[' + str + ']'
+        
+    }
+  else{
+    str = ''
+    for key in obj{
+      str += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) 
+    }
+    return '{' + str + '}'
+  }
+}
+*/
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if(typeof obj === 'string') {
+    return '"' + obj + '"';
+
+  } else if(obj === null) {
+    return 'null';
+
+  } else if (typeof obj !== 'object') {
+    return '' + obj;
+
+  } else if (Array.isArray(obj)) {
+    var str = '';
+    for(var i = 0; i < obj.length; i++) {
+      str += stringifyJSON(obj[i]) + ','
+    }
+    return '[' + str.slice(0, str.length -1) + ']'
+
+  } else {
+    var str = '';
+    for(var key in obj) {
+      if(typeof obj[key] === 'function' || typeof obj[key] === 'undefined'){
+        continue
+      }
+      str += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ','
+    }
+    return '{' + str.slice(0, str.length - 1) + '}';
+  }
+
 };
